a25f5914c1a697f85f37f33f6d75f0973da24d79
Andrey Novikov <envek@envek.name>
2022-05-26 11:22:29 +0300

    `bundle gem yabeda-activerecord`

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2022-05-26
+
+- Initial release
---
+++ spec/yabeda/activerecord_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Yabeda::Activerecord do
+  it "has a version number" do
+    expect(Yabeda::Activerecord::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
